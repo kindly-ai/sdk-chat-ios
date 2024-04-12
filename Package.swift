22017ec863ef0525eb485bb1da910b826b2694b1
Alexy <alexy.ib@outlook.com>
2024-04-12 14:59:56 +0300

    🔧 Update package dependencies version
    Adjusted package dependencies versions and targets in Package.swift for KindlySDK.

--- Package.swift
+++ Package.swift
@@ -5,62 +5,64 @@ import PackageDescription
 
 let package = Package(
     name: "KindlySDK",
-	platforms: [
-		.iOS(.v12),
-	],
+    platforms: [
+        .iOS(.v12),
+    ],
     products: [
         .library(
             name: "KindlySDK",
-            targets: ["KindlySDK", "KindlySDKWrapper"]),
+            targets: ["KindlySDK", "KindlySDKWrapper"]
+        ),
     ],
-	dependencies: [
-		.package(
-			url: "https://github.com/daltoniam/Starscream.git",
-			exact: "4.0.6"
-		),
-		.package(
-			url: "https://github.com/kirualex/SwiftyGif.git",
-			exact: "5.4.4"
-		),
-		.package(
-			url: "https://github.com/SnapKit/SnapKit.git",
-			exact: "5.7.0"
-		),
-		.package(
-			url: "https://github.com/SwiftyJSON/SwiftyJSON.git",
-			exact: "5.0.1"
-		),
-	],
+    dependencies: [
+        .package(
+            url: "https://github.com/daltoniam/Starscream.git",
+            exact: "4.0.6"
+        ),
+        .package(
+            url: "https://github.com/kirualex/SwiftyGif.git",
+            exact: "5.4.4"
+        ),
+        .package(
+            url: "https://github.com/SnapKit/SnapKit.git",
+            exact: "5.6.0"
+        ),
+        .package(
+            url: "https://github.com/SwiftyJSON/SwiftyJSON.git",
+            exact: "5.0.1"
+        ),
+    ],
     targets: [
-		.binaryTarget(name: "KindlySDK", path: "artifacts/Kindly.xcframework"),
-		.target(
-			name: "KindlySDKWrapper",
-			dependencies: [
-				.product(
-					name: "Starscream",
-					package: "Starscream",
-					condition: .when(platforms: [.iOS])
-				),
-				.product(
-					name: "SwiftyGif",
-					package: "SwiftyGif",
-					condition: .when(platforms: [.iOS])
-				),
-				.product(
-					name: "SnapKit",
-					package: "SnapKit",
-					condition: .when(platforms: [.iOS])
-				),
-				.product(
-					name: "SwiftyJSON",
-					package: "SwiftyJSON",
-					condition: .when(platforms: [.iOS])
-				),
-			],
-			path: "Sources"
-		),
+        .binaryTarget(name: "KindlySDK", path: "artifacts/Kindly.xcframework"),
+        .target(
+            name: "KindlySDKWrapper",
+            dependencies: [
+                .product(
+                    name: "Starscream",
+                    package: "Starscream",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .product(
+                    name: "SwiftyGif",
+                    package: "SwiftyGif",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .product(
+                    name: "SnapKit",
+                    package: "SnapKit",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .product(
+                    name: "SwiftyJSON",
+                    package: "SwiftyJSON",
+                    condition: .when(platforms: [.iOS])
+                ),
+            ],
+            path: "Sources"
+        ),
         .testTarget(
             name: "KindlySDKTests",
-            dependencies: ["KindlySDK"]),
+            dependencies: ["KindlySDK"]
+        ),
     ]
 )
