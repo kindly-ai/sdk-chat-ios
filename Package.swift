68460d0b2f60f1f11e601afd8d63fe87112a1eb8
Alexy <alexy.ib@gmail.com>
2024-08-26 17:10:49 +0300

    Add dependencies and frameworks for KindlySDK

--- Package.swift
+++ Package.swift
@@ -15,48 +15,48 @@ let package = Package(
         ),
     ],
     dependencies: [
-//        .package(
-//            url: "https://github.com/daltoniam/Starscream.git",
-//            exact: "4.0.6"
-//        ),
-//        .package(
-//            url: "https://github.com/kirualex/SwiftyGif.git",
-//            exact: "5.4.4"
-//        ),
-//        .package(
-//            url: "https://github.com/SwiftyJSON/SwiftyJSON.git",
-//            exact: "5.0.1"
-//        ),
-//        .package(
-//            url: "https://github.com/getsentry/sentry-cocoa.git",
-//            from: "8.30.0"
-//        ),
+        .package(
+            url: "https://github.com/daltoniam/Starscream.git",
+            exact: "4.0.6"
+        ),
+        .package(
+            url: "https://github.com/kirualex/SwiftyGif.git",
+            exact: "5.4.4"
+        ),
+        .package(
+            url: "https://github.com/SwiftyJSON/SwiftyJSON.git",
+            exact: "5.0.1"
+        ),
+        .package(
+            url: "https://github.com/getsentry/sentry-cocoa.git",
+            from: "8.30.0"
+        ),
     ],
     targets: [
         .binaryTarget(name: "KindlySDK", path: "artifacts/Kindly.xcframework"),
         .target(
             name: "KindlySDKWrapper",
             dependencies: [
-//                .product(
-//                    name: "Starscream",
-//                    package: "Starscream",
-//                    condition: .when(platforms: [.iOS])
-//                ),
-//                .product(
-//                    name: "SwiftyGif",
-//                    package: "SwiftyGif",
-//                    condition: .when(platforms: [.iOS])
-//                ),
-//                .product(
-//                    name: "SwiftyJSON",
-//                    package: "SwiftyJSON",
-//                    condition: .when(platforms: [.iOS])
-//                ),
-//                .product(
-//                    name: "Sentry",
-//                    package: "sentry-cocoa",
-//                    condition: .when(platforms: [.iOS])
-//                ),
+                .product(
+                    name: "Starscream",
+                    package: "Starscream",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .product(
+                    name: "SwiftyGif",
+                    package: "SwiftyGif",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .product(
+                    name: "SwiftyJSON",
+                    package: "SwiftyJSON",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .product(
+                    name: "Sentry",
+                    package: "sentry-cocoa",
+                    condition: .when(platforms: [.iOS])
+                ),
             ],
             path: "Sources"
         ),
